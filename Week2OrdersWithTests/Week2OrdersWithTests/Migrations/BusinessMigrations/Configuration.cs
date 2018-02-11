5a8fc24a9c35db6f1675a250065a9a86ffeff939
eimutisgenys <eimutis.genys@gmail.com>
2018-02-11 12:44:29 +0000

    Seed Data

--- Week2OrdersWithTests/Week2OrdersWithTests/Migrations/BusinessMigrations/Configuration.cs
+++ Week2OrdersWithTests/Week2OrdersWithTests/Migrations/BusinessMigrations/Configuration.cs
@@ -1,10 +1,15 @@
 namespace Week2OrdersWithTests.Migrations.BusinessMigrations
 {
+    using CsvHelper;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
+    using System.Text;
     using Week2OrdersWithTests.Models;
+    using Week2OrdersWithTests.Models.BusinessModels;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Week2OrdersWithTests.Models.BusinessDBContext>
     {
@@ -16,10 +21,64 @@ namespace Week2OrdersWithTests.Migrations.BusinessMigrations
             MigrationsDirectory = @"Migrations\BusinessMigrations";
         }
 
-        protected override void Seed(Week2OrdersWithTests.Models.BusinessDBContext context)
+        protected override void Seed(Week2OrdersWithTests.Models.BusinessDBContext c)
         {
-            SeedCustomers(context);
+            //SeedCustomers(c);
+            //SeedProducts(c); -- Not enough fields in CSV file
+            //AssignOrders(c); -- Cant assign to product that doesnt exist
         }
+
+        private void AssignOrders(BusinessDBContext c)
+        {
+            // OrderLines
+            c.OrderLines.AddOrUpdate(
+              p => p.ID,
+              new OrderLine { OrderID = 1, ProductID = 1 },
+              new OrderLine { OrderID = 2, ProductID = 1 },
+              new OrderLine { OrderID = 3, ProductID = 1 },
+              new OrderLine { OrderID = 4, ProductID = 1 }
+            );
+
+            // Orders
+            c.Orders.AddOrUpdate(
+              p => p.ID,
+              new Order { OrderDate = DateTime.Now, CustomerID = 1 },
+              new Order { OrderDate = DateTime.Now, CustomerID = 2 },
+              new Order { OrderDate = DateTime.Now, CustomerID = 3 },
+              new Order { OrderDate = DateTime.Now, CustomerID = 4 }
+            );
+        }
+
+        public void SeedProducts(BusinessDBContext ctx)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            string resourceName = "Week2OrdersWithTests.Migrations.BusinessMigrations.Products.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    var productData = csvReader.GetRecords<ProductDataImport>().ToArray();
+                    foreach (var dataItem in productData)
+                    {
+                        ctx.Products.AddOrUpdate(c =>
+                                new Product
+                                {
+                                    Description = dataItem.Description,
+                                    UnitPrice = dataItem.UnitPrice,
+                                    StockOnHand = dataItem.StockOnHand,
+                                    ReorderLevel = 1, // Missing field in CSV file
+                                    ReorderQuantity = 1 // Missing field in CSV file
+                                });
+                        // Missing field exception
+                    }
+                }
+            }
+            ctx.SaveChanges();
+        }
+
 
         private void SeedCustomers(BusinessDBContext c)
         {
---
+++ Week2OrdersWithTests/Week2OrdersWithTests/Models/BusinessModels/ProductDataImport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week2OrdersWithTests.Models.BusinessModels
+{
+    public class ProductDataImport
+    {
+        public string Description { get; set; }
+        public float UnitPrice { get; set; }
+        public int StockOnHand { get; set; }
+    }
+}
