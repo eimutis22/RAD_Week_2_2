a4199ee59dbd9086027917f687ba2db444772303
eimutisgenys <eimutis.genys@gmail.com>
2018-02-11 11:45:30 +0000

    Seed Customers with Rnd Credit within Range

--- Week2OrdersWithTests/Week2OrdersWithTests/Migrations/BusinessMigrations/Configuration.cs
+++ Week2OrdersWithTests/Week2OrdersWithTests/Migrations/BusinessMigrations/Configuration.cs
@@ -4,9 +4,12 @@ namespace Week2OrdersWithTests.Migrations.BusinessMigrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Week2OrdersWithTests.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Week2OrdersWithTests.Models.BusinessDBContext>
     {
+        Random rnd = new Random();
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
@@ -15,18 +18,23 @@ namespace Week2OrdersWithTests.Migrations.BusinessMigrations
 
         protected override void Seed(Week2OrdersWithTests.Models.BusinessDBContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            SeedCustomers(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void SeedCustomers(BusinessDBContext c)
+        {
+            c.Customers.AddOrUpdate(
+              p => p.Name,
+              new Customer { Name = "Andrew Peters", Address = "2 House Street", CreditRating = RndCredit(2000, 4000) },
+              new Customer { Name = "Brice Lambson", Address = "5 House Lane", CreditRating = RndCredit(2000, 4000) },
+              new Customer { Name = "Rowan Miller", Address = "6 House Town", CreditRating = RndCredit(2000, 4000) },
+              new Customer { Name = "John Smith", Address = "3 Apartment Block", CreditRating = RndCredit(2000, 4000) }
+            );
+        }
+
+        private float RndCredit(int a, int b)
+        {
+            return rnd.Next(a, b);
         }
     }
 }
